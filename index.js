e3bf4513067dfe7f501b953a614b26da99b2255c
jafar bino <binocoder@gmail.com>
2023-10-09 19:07:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Please read the README.md to understand what to do. Happy Coding !
+
+
+async function fetchUserData() {
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const usersData = await response.json();
+
+    
+    const modifiedUsers = usersData.map(user => ({
+      Name: user.name,
+      Username: user.username,
+      Email: user.email,
+      Address: user.address.street,
+      Phone: user.phone,
+      Website: user.website,
+      Company: user.company.name,
+    }));
+
+    
+    console.log('Modified Users Data:');
+    console.log(modifiedUsers);
+
+    
+    const filteredUsers = modifiedUsers.filter(user => user.Email.endsWith('.biz'));
+
+    
+    console.log('Filtered Users with .biz Domain Emails:');
+    console.log(filteredUsers);
+
+    
+    modifiedUsers.sort((a, b) => a.Name.localeCompare(b.Name));
+
+    
+    console.log('Sorted Users by Name:');
+    console.log(modifiedUsers);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
+
+fetchUserData();
